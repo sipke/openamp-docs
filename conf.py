f58e24a0d9988b368c7864a0440cd5e6ceecfdf0
Sipke Vriend <sipke@direktembedded.com>
2025-02-24 14:57:44 +1000

    wip: use base_url for doxylink

--- conf.py
+++ conf.py
@@ -66,7 +66,7 @@ extensions = [
 html_base = f"{pwd}/_build/html/"
 
 doxylink = {
-    'openamp_lib': (f'{pwd}/_readthedocs/openamp/doc/openamp_lib.tag', f'{pwd}/_readthedocs/openamp/doc')
+    'openamp_lib': (f'{pwd}/_readthedocs/openamp/doc/openamp_lib.tag', f'{url_base}doxygen/openamp')
 }
 
 pdf_documents = [('index', u'openamppdf', u'Sample openamppdf doc', u'Tammy Leino'),]
